9cfffa2da899d355a229dba80cbd0ac96e8e0e84
Julius Wallnöfer <j.wallnoefer@hotmail.com>
2017-04-05 12:57:51 +0200

    Food Growth, Eating and Death added

--- neural_evolution.py
+++ neural_evolution.py
@@ -11,20 +11,31 @@ import numpy as np
 from itertools import product
 from colorsys import hsv_to_rgb
 from math import radians, sin, cos
+from random import random
 pg.init()
 
 CREEP_START_FOOD = 50
 CREEP_FOOD_DECAY = 1
-WORLD_SIZE = WORLD_WIDTH, WORLD_HEIGHT = 900,900
+CREEP_FOOD_MAX = 100
+WORLD_CANVAS = WORLD_WIDTH, WORLD_HEIGHT = 900,900
+WORLD_SIZE = 30,30
 NETWORK = [1,4,2]
 CREEP_COLOR = 255,0,0
 CREEP_NOSE_COLOR = 0,0,255
 BLACK = 0,0,0
+CREEP_DEAD_COLOR = 169,169,169
 CREEP_RADIUS = 10
 CREEP_NOSE_RADIUS = 3
-CREEP_ACCELERATION = 0.5
-CREEP_MAX_SPEED = 10
+CREEP_ACCELERATION = 0.25
+CREEP_MAX_SPEED = 5
 CREEP_TURN_SPEED = 5
+CREEP_EAT_RATE = 1.5
+FOOD_GROWTH = 0.02
+WORLD_FOOD_MAX = 100
+
+clock = pg.time.Clock()
+
+world = np.ones(WORLD_SIZE)*5 + np.random.rand(*WORLD_SIZE)*10
 
 def sig(x):
     return 1/(1+np.exp(-x))
@@ -51,14 +62,19 @@ class creep:
         self.facing = facing #0-359
         self.speed = 0.
         self.food = CREEP_START_FOOD
-#        self.alive = True
+        self.alive = True
 
     
     def eat(self):
-        pass
+        i = int(self.pos[0] * WORLD_SIZE[0] / WORLD_WIDTH)
+        j = int(self.pos[1] * WORLD_SIZE[1] / WORLD_HEIGHT)
+        eat_amount = min(world[i,j],CREEP_EAT_RATE,CREEP_FOOD_MAX - self.food)
+        self.food += eat_amount
+        world[i,j] -= eat_amount
+    
         
     def die(self):
-        pass
+        creep.alive = False
     
     def move(self):
         x,y = self.pos
@@ -74,13 +90,13 @@ class creep:
         self.food -= CREEP_FOOD_DECAY
         if self.food < 0:
             self.die()
-#            return
+            return
         inputs = np.random.rand(1)
         outputs = self.brain.think(inputs)
         acc = (outputs[0] - 0.5)*2 * CREEP_ACCELERATION
         turn = (outputs[1] - 0.5)*2 * CREEP_TURN_SPEED
         new_speed = self.speed + acc
-        self.speed = clamp(-CREEP_MAX_SPEED,new_speed,CREEP_MAX_SPEED)
+        self.speed = clamp(0,new_speed,CREEP_MAX_SPEED)
         self.facing = (self.facing + turn) % 360
         self.move()
         
@@ -88,7 +104,10 @@ class creep:
     
     def draw(self,screen):
         pos = np.rint(np.array(self.pos)).astype(int)
-        pg.draw.circle(screen,CREEP_COLOR,pos,CREEP_RADIUS,0)
+        if self.alive:
+            pg.draw.circle(screen,CREEP_COLOR,pos,CREEP_RADIUS,0)
+        else:
+            pg.draw.circle(screen,CREEP_DEAD_COLOR,pos,CREEP_RADIUS,0)
         pg.draw.circle(screen,BLACK,pos,CREEP_RADIUS,2)
         phi = radians(self.facing)
         nose_pos = np.rint(pos + 0.6*CREEP_RADIUS * np.array([cos(phi),sin(phi)])).astype(int)
@@ -98,17 +117,15 @@ class creep:
 
     
 
-clock = pg.time.Clock()
 
-size = width, height = 900,900
-black = 0,0,0
 
-screen = pg.display.set_mode(size)
 
-N=10
+screen = pg.display.set_mode(WORLD_CANVAS)
+
+N=50
 
 mybrains = [brain(NETWORK,[np.random.rand(NETWORK[1],NETWORK[0])*2 - 1,np.random.rand(NETWORK[2],NETWORK[1])*2 - 1]) for i in range(N)]
-mycreeps = [creep((450,450),0,brain) for brain in mybrains]
+mycreeps = [creep((random()*WORLD_WIDTH,random()*WORLD_HEIGHT),0,br) for br in mybrains]
 
 
 def hsv_color(h,s,v): #h 0-1, s 0-1, v 0-1
@@ -116,23 +133,25 @@ def hsv_color(h,s,v): #h 0-1, s 0-1, v 0-1
 
 def draw_world(world):
     nx,ny = world.shape
-    dx = width / nx
-    dy = height / ny
+    dx = WORLD_WIDTH / nx
+    dy = WORLD_HEIGHT / ny
     for x,y in product(range(nx),range(ny)):
-        pg.draw.rect(screen,hsv_color(0.33,world[x,y],0.9),(x*dx,y*dy,dx,dy))
+        pg.draw.rect(screen,hsv_color(0.33,world[x,y]/WORLD_FOOD_MAX,0.9),(x*dx,y*dy,dx,dy))
 #        pg.draw.rect(screen,hsv_color(120,1,1),(x*dx,y*dy,dx,dy))
     return 
         
-world = np.random.rand(30,30)
+
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
         
     clock.tick(30)
-
+    
+    world = np.minimum(world + np.ones(WORLD_SIZE)*FOOD_GROWTH, np.ones(WORLD_SIZE)*WORLD_FOOD_MAX)
     draw_world(world)
     for creep in mycreeps:
-        creep.tick()
+        if creep.alive:
+            creep.tick()
         creep.draw(screen)
     pg.display.flip()
